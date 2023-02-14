47e2a73f7af8a383ce2bab9cc4457b6728e88315
Sanjuth Reddy <78399043+sanjuth@users.noreply.github.com>
2023-02-14 19:35:39 +0530

    Update students.js

--- routes/students.js
+++ routes/students.js
@@ -1,12 +1,8 @@
 const express = require('express');
 const router = express.Router();
-
-// Student model
 const Students = require('../models/students');
 
-// @route   GET /api/students/
-// @desc    Get all students
-// @access  Public
+
 router.get('/', async (req, res) => {
   try {
     const students = await Students.find({});
@@ -16,9 +12,7 @@ router.get('/', async (req, res) => {
   }
 });
 
-// @route   GET /api/students/:id
-// @desc    Get a specific student
-// @access  Public
+
 router.get('/:id', async (req, res) => {
   try {
     const student = await Students.findById(req.params.id);
@@ -28,9 +22,7 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-// @route   POST /api/students/
-// @desc    Create a student
-// @access  Public
+
 router.post('/', async (req, res) => {
   try {
     const newStudent = await Students.create({ name: req.body.name, email: req.body.email, enrollnumber: req.body.enrollnumber });
@@ -41,9 +33,7 @@ router.post('/', async (req, res) => {
 
 });
 
-// @route   PUT /api/students/:id
-// @desc    Update a student
-// @access  Public
+
 router.put('/:id', async (req, res) => {
   try {
     const updatedStudent = await Students.findByIdAndUpdate(req.params.id, req.body);
@@ -53,9 +43,8 @@ router.put('/:id', async (req, res) => {
   }
 });
 
-// @route   DELETE /api/students/:id
-// @desc    Delete a student
-// @access  Public
+
+
 router.delete('/:id', async (req, res) => {
   try {
     const removeStudent = await Students.findByIdAndRemove(req.params.id);
